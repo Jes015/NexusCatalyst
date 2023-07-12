2deaa36a9e10148676ac1de63cd57b633685273b
Jes015 <jes.18.sin.15@gmail.com>
2023-07-12 08:19:10 +0000

    fix: fix window bug resizing
    remove width and height WinBox props

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -2,21 +2,18 @@ import WinBox from 'react-winbox'
 
 interface props {
   title: string
-  width?: string
-  height?: string
-  max?: boolean
   url?: string
   children?: React.ReactElement
   setWindowVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Window = ({ title, url = '', children, setWindowVisible, width, height, max }: props) => {
+const Window = ({ title, url = '', children, setWindowVisible }: props) => {
   const handleWindowOnClose = () => {
     setWindowVisible(false)
   }
 
   return (
-    <WinBox max={max} width={width} height={height} onClose={handleWindowOnClose} title={title} icon={`${url}/favicon.ico`} {...(url !== '' ? { url } : { children })} />
+    <WinBox max onClose={handleWindowOnClose} title={title} icon={`${url}/favicon.ico`} {...(url !== '' ? { url } : { children })} />
   )
 }
 
